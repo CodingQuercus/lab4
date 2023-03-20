2cb55b6a8dd9eced05c95c94795f9aa15ed9ce16
id21afm <id21afm@cs.umu.se>
2023-03-20 12:41:55 +0100

    format

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -30,36 +30,36 @@ function ProductCard(props) {
 
     return (
         <>
-        {added &&
-        <div className="item-card" onMouseEnter={toggleShadow} onMouseLeave={toggleShadow} id={over? "" : "active"}>
-            <div className="item-image">
-                <img src={props.img} alt={props.alt}/>
-            </div>
-            <div className="item-content">
-                <Link to={props.link}>
-                    <h4>{props.title}</h4>
-                </Link>
-                <p className="item-stats">{props.stats}</p>
-                <p className="item-price"><strong>{props.price}</strong></p>
-            </div>
-            <div className="card-buttons">
-                <button className="add-favorite" onClick={addFavoriteItem}>
-                    <img src="BilderSmaller/favorite_border_black_36dp.svg" alt="Lägg till i ön skelista"
-                        className="favorite-icon" id={favorite ? "active" : ""}/>
-                    <img src="BilderSmaller/favorite_red_36dp.svg" alt="Lagt till i önskelista"
-                        className="added-favorite-icon" id={favorite ? "" : "active"}/>
-                </button>
-                <button className="add-shopping" onClick={addToCart}>
-                    <img src="BilderSmaller/add_shopping_cart_black_36dp.svg" alt="Lägg till i Varukorg" />
-                </button>
-            </div>
-        </div>}
-        {!added &&
-            <div className="added-to-cart" onMouseEnter={toggleShadow} onMouseLeave={toggleShadow}>
-                <i>&#10004;</i>
-                <p>Tillagd i varukorgen!</p>
-            </div>
-        }
+            {added &&
+                <div className="item-card" onMouseEnter={toggleShadow} onMouseLeave={toggleShadow} id={over ? "" : "active"}>
+                    <div className="item-image">
+                        <img src={props.img} alt={props.alt} />
+                    </div>
+                    <div className="item-content">
+                        <Link to={props.link}>
+                            <h4>{props.title}</h4>
+                        </Link>
+                        <p className="item-stats">{props.stats}</p>
+                        <p className="item-price"><strong>{props.price}</strong></p>
+                    </div>
+                    <div className="card-buttons">
+                        <button className="add-favorite" onClick={addFavoriteItem}>
+                            <img src="BilderSmaller/favorite_border_black_36dp.svg" alt="Lägg till i ön skelista"
+                                className="favorite-icon" id={favorite ? "active" : ""} />
+                            <img src="BilderSmaller/favorite_red_36dp.svg" alt="Lagt till i önskelista"
+                                className="added-favorite-icon" id={favorite ? "" : "active"} />
+                        </button>
+                        <button className="add-shopping" onClick={addToCart}>
+                            <img src="BilderSmaller/add_shopping_cart_black_36dp.svg" alt="Lägg till i Varukorg" />
+                        </button>
+                    </div>
+                </div>}
+            {!added &&
+                <div className="added-to-cart" onMouseEnter={toggleShadow} onMouseLeave={toggleShadow}>
+                    <i>&#10004;</i>
+                    <p>Tillagd i varukorgen!</p>
+                </div>
+            }
         </>
     )
 }
