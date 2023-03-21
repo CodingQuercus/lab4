e4976e0b2ee20a0fe760fd4c4750a7433c2e2548
id21afm <id21afm@cs.umu.se>
2023-03-21 11:59:33 +0100

    fix

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -2,7 +2,7 @@ export default function Video() {
     return (
         <main>
             <div className="video">
-                <video width="480" height="360" controls>
+                <video width="300px" height="300px" controls>
                     <source src="Video/video.mp4" type="video/mp4" />
                 </video>
             </div>
